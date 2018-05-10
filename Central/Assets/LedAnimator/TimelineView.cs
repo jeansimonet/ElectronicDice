8ab8618f368208cf9d580a889324f6db1fc92147
Olivier Basille <olivier.basille@gmail.com>
2018-05-11 00:08:13 +0200

    Added '+' button below last color animation, create new texture for each color animation, added ability to serialize in class

--- Central/Assets/LedAnimator/TimelineView.cs
+++ Central/Assets/LedAnimator/TimelineView.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimelineView : MonoBehaviour
 {
-	public int tmpNumAnims = 10;
-
 	[SerializeField]
 	float _minDuration = 1;
 	[SerializeField]
@@ -19,6 +18,8 @@ public class TimelineView : MonoBehaviour
 	RectTransform _ticksRoot = null;
 	[SerializeField]
 	RectTransform _colorAnimsRoot = null;
+	[SerializeField]
+	RectTransform _controls = null;
 	[SerializeField]
 	ColorAnimator _colorAnimPrefab = null;
 
@@ -49,10 +50,12 @@ public class TimelineView : MonoBehaviour
 			if (sprite != null)
 			{
 				var colorAnim = GameObject.Instantiate<ColorAnimator>(_colorAnimPrefab, _colorAnimsRoot);
+				_controls.SetAsLastSibling(); // Keep controls at the bottom
 				colorAnim.SetLedSprite(sprite);
 				colorAnim.GotFocus += OnColorAnimatorGotFocus;
 				colorAnim.GiveFocus();
 				colorAnim.ColorSlider.SelectHandle(colorAnim.ColorSlider.AllHandles[0]);
+				Repaint();
 			}
 		});
 	}
@@ -76,6 +79,47 @@ public class TimelineView : MonoBehaviour
 		}
 	}
 
+	public void TestSerialize()
+	{
+		var a = Serialize();
+		var str = new System.Text.StringBuilder();
+		str.AppendLine(a.duration.ToString());
+		foreach (var t in a.tracks)
+		{
+			str.Append(" * ");
+			str.Append(t.startTime);
+			str.Append(", ");
+			str.Append(t.duration);
+			str.Append(", ");
+			str.Append(t.ledIndex);
+			str.AppendLine();
+			foreach (var k in t.keyframes)
+			{
+				str.Append("    * ");
+				str.Append(k.time);
+				str.Append(", (");
+				str.Append(k.red);
+				str.Append(", ");
+				str.Append(k.green);
+				str.Append(", ");
+				str.Append(k.blue);
+				str.Append(")");
+				str.AppendLine();
+			}
+
+		}
+		Debug.Log(str.ToString());
+	}
+
+	public Animations.RGBAnimation Serialize()
+	{
+		return new Animations.RGBAnimation()
+		{
+			duration = (short)Mathf.RoundToInt(1000 * Duration),
+			tracks = GetComponentsInChildren<ColorAnimator>().Select(a => a.Serialize(Unit)).ToArray()
+		};
+	}
+
 	void OnColorAnimatorGotFocus(ColorAnimator colorAnim)
 	{
 		if (ActiveColorAnimator != colorAnim)
@@ -95,6 +139,7 @@ public class TimelineView : MonoBehaviour
 		// Update width
 		var size = rectTransf.sizeDelta;
 		size.x = _widthPadding + Unit * Duration;
+		size.y = 49 + 10 + _colorAnimsRoot.childCount * 80 + (_colorAnimsRoot.childCount - 1) * 10; //TODO
 		rectTransf.sizeDelta = size;
 
 		// Update vertical lines
@@ -131,7 +176,11 @@ public class TimelineView : MonoBehaviour
 	{
 		foreach (Transform child in _colorAnimsRoot)
 		{
-			GameObject.Destroy(child.gameObject);
+			if (child != _controls)
+			{
+				child.SetParent(null);
+				GameObject.Destroy(child.gameObject);
+			}
 		}
 
 		_widthPadding = (transform as RectTransform).rect.width - _ticksRoot.rect.width;
